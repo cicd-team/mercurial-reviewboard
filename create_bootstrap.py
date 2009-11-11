43436511408e42ff702a70fc2224293e4cb3a1b6
mdelagrange <devnull@localhost>
2009-11-11 11:32:40 -0600

    replaced setup_env.sh with bootstrap.py

---
+++ create_bootstrap.py
@@ -0,0 +1,24 @@
+import virtualenv
+
+after_install = '''
+def after_install(options, home_dir):
+    install_package(home_dir, 'Mercurial')
+    install_package(home_dir, 'mock')
+    install_package(home_dir, 'nose')
+
+def install_package(home_dir, package_name):
+    import os
+    opts = os.environ.get("INSTALL_OPTS","").split()
+    cmd = [join(home_dir, 'bin', 'easy_install')]
+    for opt in opts:
+        cmd.append(opt)
+    cmd.append(package_name)
+    print "Running cmd: %s" % cmd.__str__()
+    subprocess.call(cmd)
+'''
+
+if __name__ == '__main__':
+    script = virtualenv.create_bootstrap_script(after_install)
+    file = open('bootstrap.py', 'w')
+    file.write(script)
+    file.close()
--- setup_env.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ENV
-python virtualenv.py ENV --no-site-packages
-
-# use INSTALL_OPTS to customize easy install 
-# behavior (e.g. to point to a PyPI mirror)
-ENV/bin/easy_install $INSTALL_OPTS Mercurial
-ENV/bin/easy_install $INSTALL_OPTS nose
-ENV/bin/easy_install $INSTALL_OPTS mock
