8167e5d83283fdd57130bf13659be0d3446ed98d
mdelagrange <devnull@localhost>
2009-11-18 15:01:55 -0600

    adding test for using the outgoing flag when there are commits on multiple branches

--- mercurial_reviewboard/tests/test_outgoing.py
+++ mercurial_reviewboard/tests/test_outgoing.py
@@ -15,3 +15,18 @@ def test_outgoing(mock_send):
     
     expected = open('mercurial_reviewboard/tests/diffs/outgoing', 'r').read()
     eq_(expected, mock_send.call_args[0][4])
+    
+@patch('mercurial_reviewboard.send_review')
+def test_outgoing_with_branch(mock_send):
+    '''Test that only one change is included, despite a commit to another 
+    branch.'''
+    ui = mock_ui()
+    repo = get_repo(ui, 'two_revs_clone')
+    opts = get_initial_opts()
+    opts['outgoingrepo'] = 'mercurial_reviewboard/tests/repos/two_revs'
+    opts['outgoingchanges'] = True
+    postreview(ui, repo, '2', **opts)
+    
+    expected = open('mercurial_reviewboard/tests/diffs/outgoing_with_branch', 
+                    'r').read()
+    eq_(expected, mock_send.call_args[0][4])
