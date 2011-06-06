ecd0c857472041d2e67917ba27e5103464cb9182
mdelagrange <devnull@localhost>
2011-06-06 13:26:07 -0500

    added missing distribute bootstrap script to distribution

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author='Dennis Schoen',
     author_email='dennis.schoen@epublica.de',
     packages=['mercurial_reviewboard'],
+    scripts = ['distribute_setup.py'],
     description="A Mecurial extension which adds a post review command to "
         "post changesets for review to a Review Board server",
     long_description=open('README').read(),
